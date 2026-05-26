a979bb353f5044358e4c196bbf9140a4085cf475
Hesen Huseynov <gorbagor1522@gmail.com>
2026-05-26 05:57:29 +0400

    training

--- GitHubActionsLab.Tests/obj/Debug/net8.0/GitHubActionsLab.Tests.AssemblyInfo.cs
+++ GitHubActionsLab.Tests/obj/Debug/net8.0/GitHubActionsLab.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHubActionsLab.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+230722d114923da067276eb6cc955fbd05848a9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHubActionsLab.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitHubActionsLab.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
